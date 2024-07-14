821499568c97830c5fc4bf28d3e31aedc014bfd2
Roshan Singh bhadauriya <roshanbhadoriya178@gmail.com>
2024-07-14 21:53:12 +0530

    Day 2

---
+++ Day 2/index.js
@@ -0,0 +1,65 @@
+// Activity 1 
+// Arithnetic Operations
+console.log("Activity 1 :");
+const a = 10;
+const b = 20;
+const add = a+b;
+console.log(ans);
+
+
+const sub = b-a;
+console.log(sub);
+
+const mul = a*b;
+console.log(mul);
+
+const div = a/b;
+console.log(div);
+
+const rem = a%b;
+console.log(rem);
+
+
+
+// Activity 2 
+// Assignment Operators
+console.log("Activity 2 :");
+
+let d=10;
+  d +=1;
+console.log(d);
+let e=20;
+  e -=1;
+console.log(d);
+
+// Activity 3 
+// Comparison Operators
+console.log("Activity 3 :");
+
+console.log(`a is bigger : ${a>b}`);
+console.log(`b is bigger : ${a<b}`);
+console.log(`a is bigger : ${a>=b}`);
+console.log(`b is bigger : ${a<=b}`);
+console.log(`a is bigger : ${a==b}`);
+console.log(`b is bigger : ${a===b}`);
+
+
+// Activity 4 
+// Logical Operators
+console.log("Activity 4 :");
+
+if(a<b && b>0){
+  console.log(b);
+}
+
+if(a>=0 || b>0){
+  console.log("chal gya");
+}
+if(a!=0){
+  console.log(a);
+}
+
+// Activity 5 
+// Ternary Operators
+console.log("Activity 5 :");
+a>=0? console.log("yes") : console.log("no");
