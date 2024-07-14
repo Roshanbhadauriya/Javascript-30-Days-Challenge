6736b0385b01d499a3fcd9a809fc0c28d9912246
Roshan Singh bhadauriya <roshanbhadoriya178@gmail.com>
2024-07-14 21:21:25 +0530

    Day 1 code

---
+++ Day 1/index.js
@@ -0,0 +1,39 @@
+console.log("Activity 1 :")
+
+var num1 = 80;
+console.log(num1);
+let num2 = "Roshan";
+console.log(num2);
+const num3 = true;
+
+console.log("Activity 2 :")
+console.log(num3);
+let num4 = 1.21;
+console.log(num4);
+
+console.log("Activity 3 :")
+let obj1 = {
+  name : "roshan",
+  age :22,
+  gender: "male"
+  
+}
+
+let arr1 = [1,2,3,4,5];
+
+console.log(typeof(arr1));
+console.log(typeof(obj1));
+console.log(typeof(num3));
+console.log(typeof(num4));
+console.log(typeof(num2));
+
+console.log("Activity 4 :")
+let a =10;
+console.log(a);
+a=20;
+console.log(a);
+
+console.log("Activity 5 :")
+
+const b=1;
+b=2;
